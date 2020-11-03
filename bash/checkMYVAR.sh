088489d4ae794e3db2cd49159ec3beab1a1b8711
hiral26 <shahhiral26698@gmail.com>
2020-11-03 10:33:49 -0800

    lab3 script 1

--- bash/checkMYVAR.sh
+++ bash/checkMYVAR.sh
@@ -3,7 +3,7 @@
 # It is expected that you use this script to test if MYVAR is inherited from a parent process
 #   since it is not created or modified in this script
 
-[ -v MYVAR ] && echo "The variable MYVAR exists"
+[ -v MYVAR ] && echo "The variable MYVAR exists "
 [ -v MYVAR ] || echo "The variable MYVAR does not exist"
 
 [ -v MYVAR ] && [ -n "$MYVAR" ] && echo "The variable MYVAR has data in it"
